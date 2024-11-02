3c26f7add4a25dd192e8f0d0d2789f47dcd1d4d8
hattiza <quiet.iron8712@fastmail.com>
2024-11-02 14:36:42 -0700

    Call, fetch, and display

--- src/ccsh/main.py
+++ src/ccsh/main.py
@@ -1,5 +1,29 @@
+import subprocess
+
 def ccsh():
-  print("CCSH")
+  command = input("ccsh> ").strip()
+
+
+  result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  stdout, stderr = result.stdout, result.stderr
+  out = stdout.decode('utf-8')
+  err = stderr.decode('utf-8')
+
+
+  if stderr:
+    lines = err.split("\n")
+    for line in lines:
+      print(line)
+  else:
+    lines = out.split("\n")
+    for line in lines:
+      print(line)
+
+
+  print(stdout, stderr)
+
+
+
 
 if __name__ == "__main__":
   ccsh()
